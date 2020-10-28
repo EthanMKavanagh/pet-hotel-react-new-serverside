f814b4ff7ef55b50e531214e4c0d1cb83fb5902d
Brady Trudeau <bradytrudeau@gmail.com>
2020-10-28 11:02:46 -0500

    Adjusted Pathways

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Nav from '../Nav/Nav';
 import OwnersForm from '../OwnersForm/OwnersForm';
@@ -15,7 +14,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
