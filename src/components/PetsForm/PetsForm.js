e7dc29c4e2d85a8f02c8dcb91f6b81c86edafdd3
John Hinderer <john.hinderer16@gmail.com>
2020-10-28 16:36:28 -0500

    pet post start

--- src/components/PetsForm/PetsForm.js
+++ src/components/PetsForm/PetsForm.js
@@ -4,12 +4,36 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 
 class PetsForm extends Component {
 
+      
+  state = {
+    name: '',
+    color: '',
+    breed: ''
+  }  
+
+  handleChange = (event, propertyName) => {
+    this.setState({
+      ...this.state,
+      [propertyName]: event.target.value
+    });
+  }
+
+  // submitPet = () => {
+
+  // }
 
   render() {
-      
+    console.log('in PetsForm');
     return (
       <div>
-
+        <input placeholder="Pet Name" onChange={(event) => this.handleChange(event, 'name')}/>
+        <input placeholder="Pet Color"/>
+        <input placeholder="Pet Breed"/>
+        <select>
+          <option>Owner</option>
+          <option>Owner</option>
+        </select>
+        <button>Submit</button>
       </div>
     )
   }
